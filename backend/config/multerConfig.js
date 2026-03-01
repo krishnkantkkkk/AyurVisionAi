e466011ab80c73ad2de89ee3430b6cbe7de2e496
Krishn Kant Kumar <krishnkantkumar8@gmail.com>
2026-03-01 11:10:49 +0530

    increased the multer storage limit

--- backend/config/multerConfig.js
+++ backend/config/multerConfig.js
@@ -2,7 +2,7 @@ import multer from "multer";
 
 const storage = multer.memoryStorage();
 const upload = multer({storage, limits : {
-    fileSize : 5*1024*1024
+    fileSize : 10*1024*1024
 }});
 
 export default upload
